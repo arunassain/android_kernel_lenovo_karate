a14b56bbc5e407cba2b0272a6247967528c07320
Shoaib Anwar <Shoaib0595@gmail.com>
2018-05-13 17:25:43 +0530

    QPNP-SMB_Charger: Relax Default ICL for DCP
    Note---
    This should allow to use upto 2.1A of current properly. As per my testing, this is the max. possible current limit considering the temperature of the battery. For fastest charging speed, users will need to charge their phone using a charger capable of supplying upto 2.1A@5V.
    
    Signed-off-by: Shoaib Anwar <Shoaib0595@gmail.com>

--- drivers/power/qpnp-smbcharger.c
+++ drivers/power/qpnp-smbcharger.c
@@ -509,7 +509,7 @@ module_param_named(
 	int, S_IRUSR | S_IWUSR
 );
 
-static int smbchg_default_dcp_icl_ma = 2000;
+static int smbchg_default_dcp_icl_ma = 2100;
 module_param_named(
 	default_dcp_icl_ma, smbchg_default_dcp_icl_ma,
 	int, S_IRUSR | S_IWUSR
