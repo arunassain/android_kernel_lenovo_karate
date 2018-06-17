35fd0d2ea839b76672882c69005ac78cca603dcc
arunassain <arun.assain98@gmail.com>
2018-06-17 10:02:24 +0000

    Enable ARCH Power

--- kernel/sched/features.h
+++ kernel/sched/features.h
@@ -40,7 +40,7 @@ SCHED_FEAT(WAKEUP_PREEMPTION, true)
  * Use arch dependent cpu capacity functions
  */
 SCHED_FEAT(ARCH_CAPACITY, false)
-
+SCHED_FEAT(ARCH_POWER, true)
 SCHED_FEAT(HRTICK, false)
 SCHED_FEAT(DOUBLE_TICK, false)
 SCHED_FEAT(LB_BIAS, true)
